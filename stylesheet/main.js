000fc749e81567bb23db9be95f90992615f109e5
ifrahv <ifrah.vermeer@gmail.com>
2020-03-01 18:19:52 +0000

    a

--- stylesheet/main.js
+++ stylesheet/main.js
@@ -69,24 +69,9 @@ window.onload = function(){
 
 // START OF MENU OVERLAY //
 
-var app = function () {
-  var body = undefined;
-  var menu = undefined;
-  var menuItems = undefined;
-  var init = function init() {
-    body = document.querySelector('body');
-    menu = document.querySelector('.menu-icon');
-    menuItems = document.querySelectorAll('.nav__list-item');
-    applyListeners();
-  };
-  var applyListeners = function applyListeners() {
-    menu.addEventListener('click', function () {
-      return toggleClass(body, 'nav-active');
-    });
-  };
-  var toggleClass = function toggleClass(element, stringClass) {
-    if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-  };
-  init();
-}();
+function openMenu() {
+  document.querySelector('.overlay').classList.toggle("open");
+}
+
+document.querySelector('.btn-menu').onclick = openMenu;
 
