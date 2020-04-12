13f7202a3bbe7ee2f0ef13dc2a5454d324af4e14
ifrahv <ifrah.vermeer@gmail.com>
2020-04-12 16:36:34 +0100

    page loader broken

--- stylesheet/main.js
+++ stylesheet/main.js
@@ -88,7 +88,8 @@ window.addEventListener('scroll',scrollAppear);
 
 
 
-// background overlay  //
+// background overlay  and scroll to top//
+
 
 function bgChanger (){
   if(this.scrollY > this.innerHeight / 2){
@@ -105,12 +106,20 @@ window.addEventListener("scroll", bgChanger);
 
 // Menu  //
 
-function MenuToggle(){
-  var nav = document.getElementById('menu-bar');
-  
-}
+const preloader = document.querySelector('.preloader');
 
+const fadeEffect = setInterval(() => {
+  // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 200);
 
-
+window.addEventListener('load', fadeEffect);
 
 
