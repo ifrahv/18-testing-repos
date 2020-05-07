01cc7501be3a2ce8b9e45ab9955f74f68d6add60
ifrahv <56298423+ifrahv@users.noreply.github.com>
2020-05-07 19:11:41 +0100

    Update main.js

--- stylesheet/main.js
+++ stylesheet/main.js
@@ -213,3 +213,26 @@ TweenMax.from(".svg-wrapper", 2, {
   opacity: 0,
   ease: Expo.easeInOut
 });
+
+
+
+//smooth-scrolling
+       const body = document.body,
+                scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+                height = scrollWrap.getBoundingClientRect().height - 1,
+                speed = 0.04;
+
+            var offset = 0;
+
+            body.style.height = Math.floor(height) + "px";
+
+            function smoothScroll() {
+                offset += (window.pageYOffset - offset) * speed;
+
+                var scroll = "translateY(-" + offset + "px) translateZ(0)";
+                scrollWrap.style.transform = scroll;
+
+                callScroll = requestAnimationFrame(smoothScroll);
+            }
+
+            smoothScroll();
